b5f17a2b333f971de83b29c3890f29ee7ed55406
cuijunfeng <cuijf@haierubic.com>
2015-12-01 10:56:02 +0800

    调整sample代码

--- src/main/java/com/ilab/http/code/generator/sample/SampleMain.java
+++ src/main/java/com/ilab/http/code/generator/sample/SampleMain.java
@@ -1,10 +1,12 @@
 package com.ilab.http.code.generator.sample;
 
 
+import com.ilab.http.code.generated.LoginRequest;
 import com.ilab.http.code.generator.HttpApiCodeGenerator;
 import com.ilab.http.code.generator.annotation.HttpApiCode;
 
 import java.io.File;
+import java.util.Map;
 
 /**
  * Created by cuijfboy on 15/11/28.
@@ -20,44 +22,36 @@ public class SampleMain {
         System.out.println();
         System.out.println("Api code has been generated according to ./res/SampleLoginRequest.json");
     }
-//
-//    private static void invokeHttpApiCode() {
-//        new LoginRequest() {
-//            {
-//                LoginRequest.userName = "admin@example.com";
-//                LoginRequest.userPassword = "passw0rd";
-//                LoginRequest.token = "1234567890";
-//            }
-//
-//            @Override
-//            public boolean onResponse(int statusCode, Response data, Map<String, String> header, String body) {
-//                System.out.println();
-//                System.out.println("onResponse statusCode = " + statusCode);
-//                System.out.println("onResponse data.errorCode = " + data.errorCode);
-//                System.out.println("onResponse data.errorInfo = " + data.errorInfo);
-//                System.out.println("onResponse data.userId = " + data.userId);
-//                System.out.println("onResponse data.nickName = " + data.nickName);
-//                System.out.println("onResponse header = " + header);
-//                System.out.println("onResponse body = " + body);
-//                return true;
-//            }
-//        }.go();
-//    }
+
+    private static void invokeHttpApiCode() {
+        new LoginRequest() {
+            {
+                LoginRequest.userName = "admin@example.com";
+                LoginRequest.userPassword = "passw0rd";
+                LoginRequest.token = "1234567890";
+            }
+
+            @Override
+            public boolean onResponse(int statusCode, Response data, Map<String, String> header, String body) {
+                System.out.println();
+                System.out.println("onResponse statusCode = " + statusCode);
+                System.out.println("onResponse data.errorCode = " + data.errorCode);
+                System.out.println("onResponse data.errorInfo = " + data.errorInfo);
+                System.out.println("onResponse data.userId = " + data.userId);
+                System.out.println("onResponse data.nickName = " + data.nickName);
+                System.out.println("onResponse header = " + header);
+                System.out.println("onResponse body = " + body);
+                return true;
+            }
+        }.go();
+    }
 
     public static void main(String[] args) {
         System.out.println("\n********** main start **********");
 
-        generateHttpApiCode();
+//        generateHttpApiCode();
 
-//        invokeHttpApiCode();
-
-//        HttpApi api = new HttpApi();
-//        Gson gson = new GsonBuilder().serializeNulls().create();
-//        String a = gson.toJson(api);
-//        System.out.println(a);
-//
-//        HttpApi b = gson.fromJson(a, HttpApi.class);
-//        System.out.println(b);
+        invokeHttpApiCode();
 
         System.out.println("\n********** main finish **********");
     }
